0b8934e9088b5852c2b9c58e801f383ff26380cc
James Good <jgood@oxfordeconomics.com>
2026-04-12 11:43:39 +0100

    Register magic-auth routes via strapi.server.routes in register hook

--- src/api/report/routes/custom-routes.js
+++ src/api/report/routes/custom-routes.js
@@ -11,23 +11,5 @@ module.exports = {
         middlewares: [],
       },
     },
-    {
-      method: 'POST',
-      path: '/reports/magic-auth/request-link',
-      handler: 'magic-auth.requestLink',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
-    {
-      method: 'POST',
-      path: '/reports/magic-auth/verify',
-      handler: 'magic-auth.verifyLink',
-      config: {
-        policies: [],
-        middlewares: [],
-      },
-    },
   ],
 };
